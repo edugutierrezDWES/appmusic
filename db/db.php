e2d70eaa171854ae50947703adec3fa2e0846bd8
edugutierrezDWES <71818799+edugutierrezDWES@users.noreply.github.com>
2021-02-06 22:35:07 +0100

    Add files via upload

---
+++ db/db.php
@@ -0,0 +1,17 @@
+<?php
+echo "Inicio db.php"."<br>";
+
+	$servername = "localhost";
+	$username = "root";
+	$password = "rootroot";
+	$database = "musica";
+
+	try {
+		$conexion = new PDO("mysql:host=$servername;dbname=$database", $username, $password); 	 	 	 	 	 	
+		$conexion->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION); 	 	 	 	 	 	
+	} catch (PDOException $ex) {
+		echo $ex->getMessage(); 	 	 	 	 	 	
+	}
+	
+echo "finaliza db.php"."<br>";
+?>
---
+++ index.php
@@ -0,0 +1,7 @@
+<?php
+echo "Inicio index"."<br>";
+// Llamada al fichero que inicia la conexión a la Base de Datos
+require_once("db/db.php");
+
+echo "Fin index"."<br>";
+?>

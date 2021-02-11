e0eed21490e9b63c5550e61d021eee93a6926b4e
RodrigoCanoM <71818543+RodrigoCanoM@users.noreply.github.com>
2021-02-11 09:26:14 +0100

    Update downmusic.php

--- downmusic.php
+++ downmusic.php
@@ -1,16 +1,22 @@
  <!--Código por: Rodrigo Cano--> 
 <html>
 <head>
-<title>Alta Pedido</title>
+<title>Descarga de Musica</title>
 </head>
 <body>
 <?php
 	session_start();
-	//Llama a la BBDD
-	require_once("db/db.php");
-	//Llama al controler downmusic
-	include_once("controllers/controller_downmusic.php");
 	
+	if(isset($_SESSION["usuario"])){//Comprueba que ha iniciado sesion
+		//Llama a la BBDD
+		require_once("db/db.php");
+		//Llama al controler downmusic
+		include_once("controllers/controller_downmusic.php");
+	}
+	else{
+		echo "<br> Acceso Restringido debes hacer Login con tu usuario";
+		echo "<br><a href='pe_login.php'>Volver a pagina Login</a>";
+	}
 ?>
 
 </body>

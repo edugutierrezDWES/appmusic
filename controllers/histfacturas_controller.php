3a2dee6ce7046cb9ffddb8702d7c8d19be24f4c7
Alex Calama <71818304+Acalama@users.noreply.github.com>
2021-02-12 11:41:32 +0100

    Update and rename facturas_controller.php to histfacturas_controller.php

--- controllers/histfacturas_controller.php
+++ controllers/histfacturas_controller.php
@@ -1,10 +1,10 @@
 <?php
 echo "Inicio controller"."<br>";
 //Llamada al modelo -- Intermediario entre vista y modelo !!!
-require_once("models/facturas_model.php");
+require_once("models/histfacturas_model.php");
 
 $facturas=obtenerFacturas();
 
 //Llamada a la vista -- Intermediario entre vista y modelo !!!
-require_once("views/facturas_view.php");
+require_once("views/histfacturas_view.php");
 ?>

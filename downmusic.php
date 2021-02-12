871ca5c67281faf80a039828ea70e49b5e5165e2
RodrigoCanoM <71818543+RodrigoCanoM@users.noreply.github.com>
2021-02-12 22:29:46 +0100

    Update downmusic.php

--- downmusic.php
+++ downmusic.php
@@ -2,6 +2,7 @@
 <html>
 <head>
 <title>Descarga de Musica</title>
+<meta charset="UTF-8">
 </head>
 <body>
 <?php
@@ -12,12 +13,12 @@
 		require_once("db/db.php");
 		//Llama al controler downmusic
 		include_once("controllers/controller_downmusic.php");
+		echo "<br><br><br><a href='pe_login.php'>Volver a pagina Login</a>";
 	}
 	else{
 		echo "<br> Acceso Restringido debes hacer Login con tu usuario";
 		echo "<br><a href='pe_login.php'>Volver a pagina Login</a>";
 	}
 ?>
-
 </body>
 </html>

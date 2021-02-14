14de7a171e8551053643f653d6602aca77898001
edugutierrezDWES <71818799+edugutierrezDWES@users.noreply.github.com>
2021-02-14 23:41:17 +0100

    Delete index.php

--- index.php
+++
@@ -1,9 +0,0 @@
-<?php
-echo "Inicio index"."<br>";
-// Llamada al fichero que inicia la conexión a la Base de Datos
-require_once("db/db.php");
-
-// Llamada al controlador
-require_once("controllers/peliculas_controller.php");
-echo "Fin index"."<br>";
-?>
